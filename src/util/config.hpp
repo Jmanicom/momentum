4b85433cce249944a89fe41c6b6e51215cd47751
ymir <joshmanicom@gmail.com>
2025-12-17 17:54:24 -0800

    Added config.hpp, contains OpenGL boiler plate functions

---
+++ src/util/config.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "manimath/matvec.hpp"
+#include <vector>
+
+// Define physics constants
+const double G = 6.6743e-11; // m^3 kg^-1 s^-2
+const float c = 299792458.0; // m/s
+const double pi = 3.14159265359;
+
+
+// Window Settings Config
+const char* winName = "Momentum2D";
+const Vec2i winSize = {800, 600};
+bool gravityEnabled = true;
+
+
+// ========= OpenGL window based functions ===========
+// Create window pointer var
+GLFWwindow* window;
+inline int MTMwin(GLFWwindow*& window, Vec2i winSize)
+{
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(winSize.x, winSize.y, winName, NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    //glViewport(0.0, 0.0, winSize.x, winSize.y);     // Map coordinates to size of window, instead of (-1, 1)
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
+
+// Boiler plate start frame func for momentum
+float MTMstartframe(float& previousTime) {
+    float currentTime = glfwGetTime();
+    float dt = currentTime - previousTime;
+    previousTime = currentTime;
+        if (dt > 0.25f)
+            dt = 0.25f;
+
+    glClearColor(0.12f, 0.12f, 0.18f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    return dt;
+}
+
+// Boiler plate end frame func for momentum
+void MTMendframe() {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}

f184deb198d48d50ca67e29dd5064b6b8fd3ad40
ymir <joshmanicom@gmail.com>
2025-12-19 01:27:23 -0800

    Implemented minimal working event handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,37 @@
 #include "util/config.hpp"
-#include "util/engine.hpp"
+#include "physics/engine.hpp"
 #include "util/render.hpp"
+
+Engine sim; // Init the simulation engine
+
+static void MTMEvents(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+    if (key == GLFW_KEY_A && action == GLFW_PRESS) {
+        sim.MtmAdd(MTMCircle({0.10f, 0.5f}, 10.0f, 0.1f));
+    }
+}
 
 int main()
 {
     // Create GLFW window and corresponding context
     MTMwin(window, winSize);
-
-    Engine sim;
-
-    sim.MtmAdd(MTMCircle({0.0f, 0.5f}, 10.0f, 0.05f));
+    glfwSetKeyCallback(window, MTMEvents);
+    
+    sim.MtmAdd(MTMCircle({0.0f, 0.5f}, 10.0f, 0.1f));
+    sim.MtmAdd(MTMCircle({0.0f, 1.0f}, 10.0f, 0.1f));
     sim.MtmAdd(MTMRect({0.5f, 0.5f}, 10.0f, {0.05f, 0.05f}));
 
     // In units of seconds
     float frameStart = glfwGetTime();
-
     while (!glfwWindowShouldClose(window)) {
         // Run boilerplate start frame func
         MTMstartframe();
         const float currentTime = glfwGetTime();
 
+        // +++++ Time Tracking for updating sim physics +++++
         // Store the time elapsed since the last frame began
         accumalator += currentTime - frameStart;
         // Record the starting of this frame
@@ -29,8 +41,9 @@ int main()
         while (accumalator > dt)
             accumalator -= dt;
 
-        sim.step(dt);
 
+        // +++++ Updating physics and rendering the objects +++++
+        sim.step(dt);
         Render(sim.Objects());
         
         // Run boilerplate end frame func

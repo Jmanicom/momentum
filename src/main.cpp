1d3dc0adc8032f3b8d61b52cda8034f2db9a8e45
ymir <joshmanicom@gmail.com>
2025-12-19 00:00:24 -0800

    Setup minimal working test for using new implementations

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,38 @@
 #include "util/config.hpp"
+#include "util/engine.hpp"
+#include "util/render.hpp"
 
 int main()
 {
     // Create GLFW window and corresponding context
     MTMwin(window, winSize);
 
+    Engine sim;
 
-    // Keep track of time for sim
-    float previousTime = glfwGetTime();
+    sim.MtmAdd(MTMCircle({0.0f, 0.5f}, 10.0f, 0.05f));
+    sim.MtmAdd(MTMRect({0.5f, 0.5f}, 10.0f, {0.05f, 0.05f}));
+
+    // In units of seconds
+    float frameStart = glfwGetTime();
+
     while (!glfwWindowShouldClose(window)) {
-        // MtmStartframe returns dt, use this to update sim
-        float dt = MTMstartframe(previousTime);
+        // Run boilerplate start frame func
+        MTMstartframe();
+        const float currentTime = glfwGetTime();
 
+        // Store the time elapsed since the last frame began
+        accumalator += currentTime - frameStart;
+        // Record the starting of this frame
+        frameStart = currentTime;
+        if (accumalator > 0.2f)
+            accumalator = 0.2f; // Clamp value to avoid rendering issues
+        while (accumalator > dt)
+            accumalator -= dt;
+
+        sim.step(dt);
+
+        Render(sim.Objects());
+        
         // Run boilerplate end frame func
         MTMendframe();
     }

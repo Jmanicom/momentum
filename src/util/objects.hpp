351e7685dfe2cbbd0dca0048d9648e663b4633f3
ymir <joshmanicom@gmail.com>
2025-12-19 00:00:05 -0800

    Implemented Object struct and various rendering functions

---
+++ src/util/objects.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "config.hpp"
+
+// ========== Physics ==========
+
+enum class ShapeType {Circle, Rect};
+
+struct Circle {float radius;};
+struct Rect {Vec2f halfDim;};
+
+struct Object
+{
+    Vec2f position;
+    Vec2f velocity;
+    Vec2f force;
+    float invMass;
+    float restitution;
+
+    ShapeType shape;
+    union
+    {
+        Circle circle;
+        Rect rect;
+    };
+};
+
+Object MTMCircle(const Vec2f& pos, float mass, float radius, float rest = 0.6f)
+{
+    Object o{};
+    o.position = pos;
+    o.velocity = {0.0f, 0.0f};
+    o.force = {0.0f, 0.0f};
+
+    o.invMass = mass;
+    o.restitution = rest;
+
+    o.shape = ShapeType::Circle;
+    o.circle.radius = radius;
+
+    return o;
+}
+
+Object MTMRect(const Vec2f& pos, float mass, Vec2f halfDim, float rest = 0.6f)
+{
+    Object o{};
+    o.position = pos;
+    o.velocity = {0.0f, 0.0f};
+    o.force = {0.0f, 0.0f};
+
+    o.invMass = mass;
+    o.restitution = rest;
+
+    o.shape = ShapeType::Rect;
+    o.rect.halfDim = halfDim;
+
+    return o;
+}
---
+++ src/util/render.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "objects.hpp"
+
+// ========== Rendering ==========
+
+void DrawCircle(const Object& obj)
+{
+    const int segments = 100;
+    glBegin(GL_TRIANGLE_FAN);
+
+    glVertex2d(obj.position.x, obj.position.y);
+    for (int i = 0; i <= segments; ++i) {
+        float angle = (float)i / segments * 2.0f * pi;
+        float x = obj.position.x + obj.circle.radius * cos(angle);
+        float y = obj.position.y + obj.circle.radius * sin(angle);
+
+        glVertex2f(x, y);
+    }
+
+    glEnd();
+}
+
+void DrawRect(const Object& obj)
+{
+    Vec2f min = obj.position - obj.rect.halfDim;
+    Vec2f max = obj.position + obj.rect.halfDim;
+
+    glBegin(GL_QUADS);
+    glVertex2f(min.x, min.y);
+    glVertex2f(max.x, min.y);
+    glVertex2f(max.x, max.y);
+    glVertex2f(min.x, max.y);
+
+    glEnd();
+}
+
+void Render(const std::vector<Object>& objs)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    for (const Object& obj : objs) {
+        switch (obj.shape) {
+            case ShapeType::Circle:
+                DrawCircle(obj);
+                break;
+
+            case ShapeType::Rect:
+                DrawRect(obj);
+                break;
+        }
+    }
+}
